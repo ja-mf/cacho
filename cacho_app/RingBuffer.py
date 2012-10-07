a5c207581b335a041948c14a2b9e15ea6f241db9
Sebastian Caceres <sebastian.caceresb@gmail.com>
2012-10-06 23:31:41 -0300

    Clase dudo desicion proximas jugadas

--- cacho_app/RingBuffer.py
+++ cacho_app/RingBuffer.py
@@ -10,3 +10,18 @@ class RingBuffer:
 		if len(self.data) == self.cur:
 			self.cur=0 	
 		return self.data[self.cur]
+
+class Dudo:
+	def posibles(self, movimiento, maximo_dados):
+		movimientos_posibles = []
+		pinta = 0
+		for i in range (movimiento[0]+1, maximo_dados+1):
+			if ((movimiento[1] != 1) and (pinta == 0)):
+				for j in range (movimiento[1]+1, 7):
+					movimientos_posibles.append((i,j))
+				pinta = 1
+			else:
+				for j in range (1,7):
+					movimientos_posibles.append((i,j))
+		return movimientos_posibles
+
