dc2a7815e5c80164cf77e4cd0f7dbaf9e54d9ea9
Juanito <deeckh@gmail.com>
2012-11-11 04:38:21 -0300

    js client work

--- cacho_app/static/js/cacho.js
+++ cacho_app/static/js/cacho.js
@@ -4,6 +4,12 @@ var socket = io.connect("/game");
 var sessid;
 var user_list;
 
+var numeros = ['ningun', 'un', 'dos', 'tres', 'cuatro', 'cinco'
+					'seis', 'siete', 'ocho', 'nueve', 'diez', 'once',
+					'doce', 'trece', 'catorce', 'quince', 'dieciseis',
+					'diecisiete', 'dieciocho', 'diecinueve', 'veinte']
+var pintas = ['0', 'aces', 'tontos', 'trenes', 'cuartas', 'quintas', 'sextas']
+
 socket.on('connect', function (username) {
    $('#chat').addClass('connected');
    socket.emit('join', window.room);
@@ -38,6 +44,13 @@ socket.on('usuarios_room', function (usernames) {
 	 $.each(usernames, function(k, v) {
 		$('#userlist').append('<li id="'+ v['user_name'] +'">'+v['user_name']+' '+v['confirm']+'</li>');
 	 });
+});
+
+socket.on('jugadas_posibles', function(jugadas) {
+	$('#jugadas').empty();
+	$.each(jugadas, function(k, v) {
+		$('#jugadas').append('<option value="['+v[0]','+v[1]']">'+numeros[v[0]]+' '+pintas[v[1]]+'</option>');
+	});
 });
 
 socket.on('server_message', function (data) {
@@ -83,5 +96,9 @@ $(function () {
 	 $('#confirmar').click(function () {
 		socket.emit('confirmar', 'YES');
 //		alert('CONFIRMEICHON');
+	 });
+
+	 $('#jugar').click(function() {
+		 socket.emit('jugada', $('#jugada').val());
 	 });
 });
